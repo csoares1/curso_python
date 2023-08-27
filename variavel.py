f4fa3e6f8f51453bf41e24fe9a3ee664c7ee5ef8
csoares1 <carlardsouza@gmail.com>
2023-08-27 17:49:05 -0300

    como funciona vaiável e constantes

---
+++ variavel.py
@@ -0,0 +1,34 @@
+# age = 23
+# name = 'Guilherme'
+
+# print(f'Meu nome é {name} e eu tenho {age} anos(s) de idade.')
+
+# age,name  = 7,'Giovanna'
+
+# print(f'Meu nome é {name} e eu tenho {age} anos(s) de idade.')
+
+# # teste constante
+
+# PATH = 'home/curso'
+# DEBUG = True
+# STATES = {
+#     'SP',
+#     'RJ',
+#     'PE'
+# }
+# AMOUNT = 30.2
+nome = "Guilherme"
+idade = 41
+
+nome, idade = "Giovanna", 28
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+BRAZILIAN_STATES = {
+    'SP',
+    'RJ',
+    'PE'
+}
+print(BRAZILIAN_STATES)
+
